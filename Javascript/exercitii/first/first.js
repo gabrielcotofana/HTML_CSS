589fa48f705ab98d849d3b16e97da644ab8384c1
Gabriel Cotofana <gabrielcotofana@Gabriels-iMac.local>
2020-01-16 16:22:16 +0200

    Jsva1

---
+++ Javascript/exercitii/first/first.js
@@ -0,0 +1,14 @@
+var name = "Radu";
+var message = "Hello " + name + "!";
+var timeout = 3000;
+var isNight = true
+var nothing = null;
+
+if(isNight) alert("Noapte buna!");
+
+setTimeout(seyHello, timeout);
+    function seyHello() {
+    // alert(message);
+
+console.log(message); 
+}
